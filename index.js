34aa5061cf88456a77980f9990c130b8d36c1dd3
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2026-02-07 19:57:48 +0530

    Request Handling

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const server = http.createServer((req, res) => {
   console.log("Request Method:", req.method);
 
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Node Server");
+  if(req.url === "/") {
+    res.end("Hello, World!");
+  } else if(req.url === "/about") {
+    res.end("This is the about page.");
+    } else {
+    res.end("404 Page not found.");
+    }
+
 });
 
 server.listen(3000, () => {
