38dc90375776c4c97b817d5e98e8dd3cdab8d917
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2026-02-12 20:37:25 +0530

    query parameters

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:id/post/:postId", (req, res) => {
-  console.log(req.params);
-  res.send("User ID: " + req.params.id + ", Post ID: " + req.params.postId);
+// app.get("/user/:id/post/:postId", (req, res) => {
+//   console.log(req.params);
+//   res.send("User ID: " + req.params.id + ", Post ID: " + req.params.postId);
+// });
+app.get("/search", (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
 });
 
 app.listen(3000, () => {
