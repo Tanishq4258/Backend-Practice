71f0be1cce8751cd88d0c1f50aeff1375843f0b5
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2026-02-12 20:31:46 +0530

    route parameters

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-
-
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-    console.log(req.method, req.url);
-    next();
-});
-
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
-
-app.use((req, res) => {
-  res.status(404).send("404 Not Found");
+app.get("/user/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`User ID is ${req.params.id}`);
 });
 
 app.listen(3000, () => {
-  console.log("Express server running on port 3000");
+  console.log("Server running");
 });
