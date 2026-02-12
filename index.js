91181cb386361ee374387fc0b63e1fe80430e5be
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2026-02-12 20:33:55 +0530

    multiple route parameters

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:id", (req, res) => {
+app.get("/user/:id/post/:postId", (req, res) => {
   console.log(req.params);
-  res.send(`User ID is ${req.params.id}`);
+  res.send("User ID: " + req.params.id + ", Post ID: " + req.params.postId);
 });
 
 app.listen(3000, () => {
