183d8b596db1dc6968e74bd23f678f9ac4c1dc01
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2026-02-14 16:02:05 +0530

    Static file

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
 
-// app.get("/user/:id/post/:postId", (req, res) => {
-//   console.log(req.params);
-//   res.send("User ID: " + req.params.id + ", Post ID: " + req.params.postId);
-// });
-app.get("/search", (req, res) => {
-  console.log(req.query);
-  res.send(req.query);
-});
+// Static middleware
+app.use(express.static("public"));
 
 app.listen(3000, () => {
   console.log("Server running");
