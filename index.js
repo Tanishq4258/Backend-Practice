35e8dc666055c174264ae7b7a9f14a50d115c244
Tanishq Chhabra <tanishqchhabra98@gmail.com>
2026-02-18 21:51:15 +0530

    blocking nonblocking

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 
-// Static middleware
-app.use(express.static("public"));
+
+app.get("/blocking", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+
+  const data = fs.readFileSync(filePath);
+
+  res.set("Content-Type", "text/html");  // VERY IMPORTANT
+  res.send(data);
+});
+
+
+app.get("/nonblocking", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      return res.status(500).send("Error reading file");
+    }
+
+    res.set("Content-Type", "text/html");  // VERY IMPORTANT
+    res.send(data);
+  });
+});
+
 
 app.listen(3000, () => {
-  console.log("Server running");
+  console.log("Server running on port 3000");
 });
